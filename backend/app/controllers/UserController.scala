b571b8323c18564ed3f2d0cccaaa73e3269f129d
kam_ <kamil.amrani@heig-vd.ch>
2019-05-27 15:29:16 +0200

    add logout rout and template for connextion tokens

--- backend/app/controllers/UserController.scala
+++ backend/app/controllers/UserController.scala
@@ -9,15 +9,20 @@ class UserController @Inject()(cc: ControllerComponents) extends AbstractControl
 
   case class RegisterBody(username: String, password: String)
   case class LoginBody(username: String, password: String)
+  case class LogoutBody(token: String)
 
   def register() = Action { implicit request =>
     val rb: RegisterBody = gson.fromJson(request.body.asJson.mkString, classOf[RegisterBody])
-
     Ok(s"you are trying to register with username: ${rb.username} and password: ${rb.password}")// todo
   }
 
   def login() = Action { implicit request =>
     val rb: LoginBody = gson.fromJson(request.body.asJson.mkString, classOf[LoginBody])
     Ok(s"you are trying to login with username: ${rb.username} and password: ${rb.password}")// todo
+  }
+
+  def logout() = Action { implicit request =>
+    val rb: LogoutBody = gson.fromJson(request.body.asJson.mkString, classOf[LogoutBody])
+    Ok(s"you are trying to logout with token: ${rb.token}")// todo
   }
 }
--- backend/app/services/Utility.scala
+++ backend/app/services/Utility.scala
@@ -2,6 +2,14 @@ package services
 
 import com.google.gson.Gson
 
+import scala.collection.immutable.HashMap
+
 object Utility {
   val gson = new Gson
+  var userTokens: Map[String, String] = new HashMap()//[token, username]
+
+  def generateTokenForUser(username: String): Unit = userTokens += ("new token" -> username)// todo token generator
+  def getUserFromToken(token: String): Option[String] = userTokens.get(token)
+  def removeTokenOfUser(token: String): Unit = userTokens -= token
+  def removeAllTokensOfUser(username: String): Unit = userTokens = userTokens.filter(_._2.equals(username))
 }
