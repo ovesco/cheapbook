1ccf9b6252a502c1d42557b3038a9bfb600866e3
kam_ <kamil.amrani@heig-vd.ch>
2019-06-08 17:05:37 +0200

    run endpoint WIP

--- backend/app/services/Execute.scala
+++ backend/app/services/Execute.scala
@@ -7,8 +7,9 @@ import scala.sys.process._
 
 object Execute {
   val ROOT_FOLDER: String = System.getProperty("user.home") + "/Documents/cheapbook"
-  val BUILD_FILE: String = "scalaVersion := \"2.12.8\"\n\nname := \"hello-world\"\norganization := \"ch.epfl.scala\"\nversion := \"1.0\"\n\nlibraryDependencies += \"org.typelevel\" %% \"cats-core\" % \"1.6.0\""
-  val MAIN_FILE: String = "object Main extends App {\n  println(\"Hello, World!\")\n}"
+  val BUILD_FILE_START: String = "scalaVersion := \"2.12.8\"\n\nname := \"hello-world\"\norganization := \"ch.epfl.scala\"\nversion := \"1.0\"\n\nlibraryDependencies += \"org.typelevel\" %% \"cats-core\" % \"1.6.0\"\n"
+  val MAIN_FILE_START: String = "object Main extends App {\n"
+  val MAIN_FILE_END: String = "\n}"
 
   var threads: Map[(Long, Long), Thread] = new HashMap()//[(userId, envId), thread]
   var lastResults: Map[(Long, Long), String] = new HashMap()//[(userId, envId), last result]
@@ -26,24 +27,31 @@ object Execute {
 
   def createProject(userId: Long, envId: Long) = {
     val path = ROOT_FOLDER + "/" + userId + "/" + envId
-    val docs = new File(path)
-    if (!docs.exists()) docs.mkdirs()
-    if (!new File(path + "/hello").exists()) {
-      new File(path + "/hello/src/main/scala/").mkdirs()
-      val buildFile = new PrintWriter(new File(path + "/hello/build.sbt"))
-      buildFile.write(BUILD_FILE)
-      val mainFile = new PrintWriter(new File(path + "/hello/src/main/scala/Main.scala"))
-      mainFile.write(MAIN_FILE)
-    }
+    if (!new File(path + "/hello").exists()) new File(path + "/hello/src/main/scala/").mkdirs()
 
-    //todo write code ?
+    //todo write code and dependencies
+    val code = "println(\"Hello World\")"
+    val deps = List("", "")
+
+    val buildFile = new PrintWriter(new File(path + "/hello/build.sbt"))
+    buildFile.write(BUILD_FILE_START)
+    deps.foreach(d => buildFile.write(d + "\n"))
+    buildFile.flush()
+    buildFile.close()
+    val mainFile = new PrintWriter(new File(path + "/hello/src/main/scala/Main.scala"))
+    mainFile.write(MAIN_FILE_START)
+    mainFile.write(code)
+    mainFile.write(MAIN_FILE_END)
+    mainFile.flush()
+    mainFile.close()
   }
 
   def addThread(userId: Long, envId: Long): Boolean = {
     if (threads.contains((userId, envId))) {
       false
     } else {
-      threads += ((userId, envId) -> createNewThread(userId, envId, "cmd.exe dir"))// todo
+      val cmd = "cmd.exe /c cd " + ROOT_FOLDER + "/" + userId + "/" + envId + "/hello" + " & sbt run"
+      threads += ((userId, envId) -> createNewThread(userId, envId, cmd))// todo
       true
     }
   }
@@ -63,8 +71,13 @@ object Execute {
   def createNewThread(userId: Long, envId: Long, cmd: String): Thread = {
     new Thread(() => {
       createProject(userId, envId)
-      val output = cmd.!!
-      lastResults += ((userId, envId) -> output)
+      try {
+        val output = cmd.!!
+        val result = output.split("\n").filter(s => !s.startsWith("[info] ") && !s.startsWith("[success] ")).mkString("\n")
+        lastResults += ((userId, envId) -> result)
+      } catch {
+        case _: Exception => lastResults += ((userId, envId) -> "Error !")
+      }
       removeThread(userId, envId)
     })
   }
