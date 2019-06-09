943c0841ee8a3611ddb4093117490abbb084f58b
kam_ <kamil.amrani@heig-vd.ch>
2019-06-09 16:13:28 +0200

    exe wip results to Options

--- backend/app/services/Execute.scala
+++ backend/app/services/Execute.scala
@@ -3,7 +3,6 @@ package services
 import java.io.{File, PrintWriter}
 
 import scala.collection.immutable.HashMap
-import scala.concurrent.Future
 import scala.sys.process._
 
 object Execute {
@@ -13,10 +12,10 @@ object Execute {
   val MAIN_FILE_END: String = "\n}"
 
   var threads: Map[(Long, Long), Thread] = new HashMap()//[(userId, envId), thread]
-  var lastResults: Map[(Long, Long), String] = new HashMap()//[(userId, envId), last result]
+  var lastResults: Map[(Long, Long), Option[String]] = new HashMap()//[(userId, envId), last result]
 
-  def run(userId: Long, envId: Long, code: Future[Option[Model.Environnement]], deps: Future[Seq[Model.Dependencies]]) = {
-    if (addThread(userId, envId)) startThread(userId, envId)
+  def run(userId: Long, envId: Long, env: Model.Environnement, deps: Seq[Model.Dependencies]) = {
+    if (addThread(userId, envId, env, deps)) startThread(userId, envId)
     threads(userId, envId).join()
     lastResults(userId, envId)
   }
@@ -26,13 +25,13 @@ object Execute {
     removeThread(userId, envId)
   }
 
-  def createProject(userId: Long, envId: Long) = {
+  def createProject(userId: Long, envId: Long, env: Model.Environnement, deps: Seq[Model.Dependencies]) = {
     val path = ROOT_FOLDER + "/" + userId + "/" + envId
     if (!new File(path + "/hello").exists()) new File(path + "/hello/src/main/scala/").mkdirs()
 
     //todo write code and dependencies
-    val code = "println(\"Hello World\")"
-    val deps = List("", "")
+    val code = env.code//"println(\"Hello World\")"
+    //val deps = List("", "")
 
     val buildFile = new PrintWriter(new File(path + "/hello/build.sbt"))
     buildFile.write(BUILD_FILE_START)
@@ -47,12 +46,12 @@ object Execute {
     mainFile.close()
   }
 
-  def addThread(userId: Long, envId: Long): Boolean = {
+  def addThread(userId: Long, envId: Long, env: Model.Environnement, deps: Seq[Model.Dependencies]): Boolean = {
     if (threads.contains((userId, envId))) {
       false
     } else {
       val cmd = "cmd.exe /c cd " + ROOT_FOLDER + "/" + userId + "/" + envId + "/hello" + " & sbt run"
-      threads += ((userId, envId) -> createNewThread(userId, envId, cmd))// todo
+      threads += ((userId, envId) -> createNewThread(userId, envId, cmd, env, deps))
       true
     }
   }
@@ -69,15 +68,15 @@ object Execute {
     threads -= ((userId, envId))
   }
 
-  def createNewThread(userId: Long, envId: Long, cmd: String): Thread = {
+  def createNewThread(userId: Long, envId: Long, cmd: String, env: Model.Environnement, deps: Seq[Model.Dependencies]): Thread = {
     new Thread(() => {
-      createProject(userId, envId)
+      createProject(userId, envId, env, deps)
       try {
         val output = cmd.!!
         val result = output.split("\n").filter(s => !s.startsWith("[info] ") && !s.startsWith("[success] ")).mkString("\n")
-        lastResults += ((userId, envId) -> result)
+        lastResults += ((userId, envId) -> Some(result))
       } catch {
-        case _: Exception => lastResults += ((userId, envId) -> "Error !")
+        case _: Exception => lastResults += ((userId, envId) -> Option.empty)
       }
       removeThread(userId, envId)
     })
