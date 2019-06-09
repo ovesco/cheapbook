62e9b3d5006d41ab29e94e87da977259566e013b
Florent <shinopill@gmail.com>
2019-06-09 15:54:14 +0200

    Change ID for token

--- backend/app/controllers/ExeController.scala
+++ backend/app/controllers/ExeController.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import DAO.{DependenciesDAO, EnvironnementDAO, UsersDAO}
-import Model.Environnement
 import javax.inject.{Inject, Singleton}
 import play.api.mvc.{AbstractController, ControllerComponents}
 import services.Utility.gson
@@ -13,17 +12,17 @@ class ExeController @Inject()(cc: ControllerComponents,
                               depDao : DependenciesDAO,
                               userDao: UsersDAO ) extends AbstractController(cc) {
 
-  case class runBody(userId : Long, envId : Long)
-  case class stopBody(userId : Long, envId : Long)
+  case class runBody(token : String, envId : Long)
+  case class stopBody(token : String, envId : Long)
   def run() = Action { implicit request =>
     val body : runBody = gson.fromJson(request.body.asJson.mkString,classOf[runBody])
-    val result = Execute.run(body.userId,body.envId)
+    val result = Execute.run(Utility.getUserFromToken(body.token).get,body.envId)
     Ok(s"$result")
   }
 
   def stop() = Action { implicit request =>
     val body : stopBody = gson.fromJson(request.body.asJson.mkString,classOf[stopBody])
-    val result = Execute.stop(body.userId,body.envId)
+    val result = Execute.stop(Utility.getUserFromToken(body.token).get,body.envId)
     Ok(s"$result")
   }
 }
